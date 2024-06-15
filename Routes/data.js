a39594ea985794d211e2173a677da5dac470393d
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-06-15 12:04:45 +0530

    Add

--- Routes/data.js
+++ Routes/data.js
@@ -3,8 +3,11 @@ import * as dotenv from 'dotenv';
 import Post from '../mongoose/model/Data.js';
 import axios from 'axios';
 import crypto from 'crypto'
+import CryptoJS from 'crypto-js';
 import { request } from 'http';
-import sha256 from 'sha256';
+// import sha256 from 'sha256';
+import { error } from 'console';
+// import sha256 from 'sha256';
 dotenv.config();
 const router = express.Router();
 // router.route('/').get((req,res)=>{
@@ -20,73 +23,159 @@ router.route('/').get(async (req, res) => {
   }
 });
 
-router.route('/payment').post(async (req, res) => {
+router.route('/payments').post(async (req, res) => {
+  try{
   const MUID='MUID'+Date.now();
+
   const transactionId='T'+Date.now()
  const data= {
     merchantId: "M225WZIY10UMV",
     merchantTransactionId: 'T'+Date.now(),
-    merchantUserId:'MUID'+Date.now(),
-    amount: 200,
-    name:"shivu",
-    // email:"Shivu369sapare@gmail.com",
-    redirectUrl: `http://localhost:8080/api/v1/status/${'T'+Date.now()}`,
-     redirectMode:'POST',
-    mobileNumber: "9380309188",
-  paymentInstrument : {
-      type: "PAY_PAGE",
-      
-    }
+    merchantUserId: 'MUID' + Date.now(),
+      name: "shivu",
+      amount: 2 * 100,
+      redirectUrl: `http://localhost:3001/api/v1/status/${'T'+Date.now()}`,
+      redirectMode: "POST",
+      mobileNumber:'9380309188',
+      paymentInstrument: {
+        type: "PAY_PAGE",
+      },
   }
-  const payload=JSON.stringify(data);
-  const payloadMain=Buffer.from(JSON.stringify(data),'utf8').toString('base64')
-  const keyIndex=1;
-  const xVarify=sha256(payloadMain+'/pg/v1/pay'+'a1af0450-4fce-4c51-bded-29c7c5affd8e')+'###'+keyIndex;
+  // const payload=JSON.stringify(data);
+  // const payloadMain=Buffer.from(JSON.stringify(data),'utf8').toString('base64')
+  // const keyIndex=1;
+  // // const xVarify=sha256(payloadMain+'/pg/v1/pay'+'a1af0450-4fce-4c51-bded-29c7c5affd8e')+'###'+keyIndex;
   // const string=payloadMain+'/pg/v1/pay'+'a1af0450-4fce-4c51-bded-29c7c5affd8e';
   // const sha256=crypto.createHash('sha256').update(string).digest('hex')
   // const checkSum=sha256+'###'+keyIndex;
   const prod_url=`https://api.phonepe.com/apis/hermes/pg/v1/pay`
-  // const options = {
-  //   method:'POST',
-  //   url:prod_url,
-  //   headers:{
-  //     accept:'application/json',
-  //     'Content-Type':'application/json',
-  //     'X-VERIFY':checkSum,
-  //   },
-  //   data:{
-  //     request:payloadMain
-  //   }
-  // }
 
-  // axios.request(options).then((res)=>{
-  //   console.log(res.data.data.instrumentResponse.redirectInfo.url);
-  //   // return res.status(200).send(res.data.data.instrumentResponse.redirectInfo.url)
-  // })
+  const payload = JSON.stringify(data);
+  const payloadMain = Buffer.from(payload).toString("base64");
+
+  const key = "a1af0450-4fce-4c51-bded-29c7c5affd8e";
+  const keyIndex = 1;
+  const string = payloadMain + "/pg/v1/pay" + key;
+  const sha256 = CryptoJS.SHA256(string).toString();
+  const checksum = sha256 + "###" + keyIndex;
   const options = {
-    method:'post',
+    method:'POST',
     url:prod_url,
     headers:{
       accept:'application/json',
       'Content-Type':'application/json',
-      'X-VERIFY':xVarify,
+      'X-VERIFY':checksum,
     },
     data:{
       request:payloadMain
     }
   };
+  
   axios
     .request(options)
         .then(function (response) {
         // console.log(response.data);
-        res.send(response.data.data.instrumentResponse.redirectInfo.url);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+        res.status(201).send({
+          msg: "payment done",
+          status: "success",
+          data: response.data,
+         
+        });
+        console.log(response.data.data.instrumentResponse.redirectInfo.url);
+      })
+     .catch(function (error) {
+        console.error("Payment API Error:", error.message);
+        res.status(500).json({ msg: "Payment Failed", status: "error", error: error.message });
+      });
+  } catch (e) {
+    console.error("Internal Server Error:", e.message);
+    res.status(500).json({ msg: error, status: "error", error: e.message });
+  }  
+     
 })
 
+router.post("/payment", async (req, res) => {
+  // console.log(req.body);
 
+  try {
+    // const price = parseFloat(req.body.price);
+    // const { user_id, phone, name, email, tempId } = req.body;
+
+    // Set the values to variables for later use
+
+    // this.user = user_id;
+    
+    // this.tempId = tempId;
+    // this.price = price;
+
+    const data = {
+      merchantId: "PGTESTPAYUAT86",
+      merchantTransactionId: generatedTranscId(),
+      merchantUserId: 'MUID' + Date.now(),
+      name: "shivu",
+      amount: 2 * 100,
+      redirectUrl: `http://localhost:3001/api/v1/status/${generatedTranscId()}`,
+      redirectMode: "POST",
+      mobileNumber:'9380309188',
+      paymentInstrument: {
+        type: "PAY_PAGE",
+      },
+    };
+
+    const payload = JSON.stringify(data);
+    const payloadMain = Buffer.from(payload).toString("base64");
+
+    const key = "96434309-7796-489d-8924-ab56988a6076";
+    const keyIndex = 1;
+    const string = payloadMain + "/pg/v1/pay" + key;
+    const sha256 = CryptoJS.SHA256(string).toString();
+    const checksum = sha256 + "###" + keyIndex;
+    // const payloadMain=Buffer.from(JSON.stringify(data),'utf8').toString('base64')
+;
+    // const xVarify=sha256(payloadMain+'/pg/v1/pay'+'a1af0450-4fce-4c51-bded-29c7c5affd8e')+'###'+keyIndex;
+    // const string=payloadMain+'/pg/v1/pay'+'a1af0450-4fce-4c51-bded-29c7c5affd8e';
+    // const sha256=crypto.createHash('sha256').update(string).digest('hex')
+    // const checkSum=sha256+'###'+keyIndex;
+    const prod_URL = "https://api-preprod.phonepe.com/apis/hermes/pg/v1/pay";
+    const requestData = {
+      method: "POST",
+      url: prod_URL,
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        "X-VERIFY": checksum,
+      },
+      data: {
+        request: payloadMain,
+      },
+    };
+
+    axios.request(requestData)
+     .then(async function (response) {
+        const phonePeTransactionId = response.data.transactionId;
+        res.status(201).send({
+          msg: "payment done",
+          status: "success",
+          data: response.data,
+          phonePeTransactionId: phonePeTransactionId,
+        });
+        console.log("Payment API Response:", response.data);
+        console.log(response.data.data.instrumentResponse.redirectInfo.url);
+      })
+     .catch(function (error) {
+        console.error("Payment API Error:", error.message);
+        res.status(500).json({ msg: "Payment Failed", status: "error", error: error.message });
+      });
+  } catch (e) {
+    console.error("Internal Server Error:", e.message);
+    res.status(500).json({ msg: error, status: "error", error: e.message });
+  }
+});
+
+// Function to generate a unique transaction ID
+function generatedTranscId() {
+  return 'T' + Date.now();
+}
 router.route('/status/:transactionId').post(async (req, res) => {
   const MTID=res.req.body.transactionId;
   const MId=res.req.body.marchantId;
